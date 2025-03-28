a88dc3724d1ddd0d4215b0ee1f555b84ebb9fa69
wojciechbojakowski <wojto867@gmail.com>
2025-03-28 09:01:16 +0100

    added sim_SIS.py

--- sim_SIS.py
+++ sim_SIS.py
@@ -12,13 +12,12 @@ grid = np.zeros((size, size), dtype=int)
 
 init_infected = np.random.choice(range(size*size), number_patient0, replace=False)
 for id in init_infected:
-    grid[id // size, id % size] = 1 # 0=S, 1=I, 2=R
+    grid[id // size, id % size] = 1 # 0=S, 1=I
 
 grid2 = grid.copy()
 
 all_S=size*size-number_patient0
 all_I=number_patient0
-all_R=0
 iterator=0
 
 #pierwsza wersja bez animacji
@@ -31,7 +30,7 @@ def while_func():
                         if np.random.rand() < recovery_prob:
                             grid2[i, j] = 2  #Infected->R
                             all_I=all_I-1
-                            all_R=all_R+1
+                            all_S+=1
                     elif(grid[i,j]==0):
                         acumulate_prop = 0
                         for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:#dół, góra, lewo, prawo
@@ -53,7 +52,7 @@ def while_func():
                         if np.random.rand() < recovery_prob:
                             grid[i, j] = 2  #Infected->R
                             all_I=all_I-1
-                            all_R=all_R+1
+                            all_S+=1
                     elif(grid2[i,j]==0):
                         acumulate_prop = 0
                         for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:#dół, góra, lewo, prawo
@@ -72,10 +71,10 @@ def while_func():
                             grid[i, j] = 1#zachorowanie
                             all_S=all_S-1
                             all_I=all_I+1
-        print(f"S {all_S}, I {all_I}, R {all_R}, sum {all_S+all_I+all_R}")
+        print(f"S {all_S}, I {all_I}, sum {all_S+all_I}")
         iterator=iterator+1
 
-h_S, h_I, h_R = [], [], [] # tablice na liczbe osób w czasie
+h_S, h_I = [], [] # tablice na liczbe osób w czasie
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5)) #dwa wykresy
 
 #animacja w matplotlib
@@ -86,10 +85,9 @@ im = ax1.imshow(grid, cmap=cmap, vmin=0, vmax=2)
 legend_text = ax1.text(2, 2, '', color='white', fontsize=12, bbox=dict(facecolor='black', alpha=0.5))
 susceptible_patch = mpatches.Patch(color=cmap(0.0), label=" (S)")
 infected_patch = mpatches.Patch(color=cmap(0.5), label=" (I)")
-recovered_patch = mpatches.Patch(color=cmap(1.0), label=" (R)")
-ax1.set_title("Symulacja SIR")
+ax1.set_title("Symulacja SIS")
 
-ax1.legend(handles=[susceptible_patch, infected_patch, recovered_patch], loc="upper right")
+ax1.legend(handles=[susceptible_patch, infected_patch], loc="upper right")
 
 ax2.set_xlim(0, 200)  #(liczba iteracji)
 ax2.set_ylim(0, size * size)  #(liczba osób)
@@ -99,7 +97,6 @@ ax2.set_ylabel("Liczba osób")
 
 line_S, = ax2.plot([], [], label="S ", color='b')
 line_I, = ax2.plot([], [], label="I ", color='g')
-line_R, = ax2.plot([], [], label="R ", color='r')
 ax2.legend()
 
 def update(frame):
@@ -107,9 +104,9 @@ def update(frame):
         ani.event_source.stop()
         return
     global grid
-    global all_R, all_I, all_S
-    global h_S,h_I,h_R
-    #global line_S,line_I,line_R
+    global all_I, all_S
+    global h_S,h_I
+    #global line_S,line_I
 
     new_grid = grid.copy()
 
@@ -117,9 +114,9 @@ def update(frame):
         for j in range(size):
             if grid[i, j] == 1:  # if I
                 if np.random.rand() < recovery_prob:
-                    new_grid[i, j] = 2  #I->R
+                    new_grid[i, j] = 0  #I->S
                     all_I-=1
-                    all_R+=1
+                    all_S+=1
             elif grid[i, j] == 0:  # if S
                 infected_neighbors = 0
                 for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
@@ -135,16 +132,14 @@ def update(frame):
     #print(f"S {all_S}, I {all_I}, R {all_R}, sum {all_S+all_I+all_R}")
     h_S.append(all_S)
     h_I.append(all_I)
-    h_R.append(all_R)
     line_S.set_data(range(len(h_S)), h_S)
     line_I.set_data(range(len(h_I)), h_I)
-    line_R.set_data(range(len(h_R)), h_R)
     ax2.set_xlim(0, max(200, len(h_S) + 10))
 
-    legend_text.set_text(f' S: {all_S} \n I: {all_I} \n R: {all_R} ')
+    legend_text.set_text(f' S: {all_S} \n I: {all_I}')
     grid = new_grid
     im.set_array(grid)
-    return [im, line_S, line_I, line_R]
+    return [im, line_S, line_I]
 
 ani = animation.FuncAnimation(fig, update, frames=200, interval=100, blit=False, repeat=False)
 plt.show()
